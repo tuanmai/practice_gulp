bc70e9cd5ce7e431e0e4a54c93d6d6ceec6006ea
Tuan Mai <tuan.mai@eastagile.com>
2015-04-07 00:52:11 +0700

    Add event handler for compile error

--- gulpfile.js
+++ gulpfile.js
@@ -19,26 +19,41 @@ var dest_css    = "dist/css"
 var src_js      = "src/js/**/*.js"
 var src_sass    = "src/sass/**/*.scss"
 
+var onError = function(err) {
+  console.log(err);
+  this.emit('end');
+}
+
 
 // ----------------------------------------------------------------
 
 // SASS to CSS
 gulp.task('sass', function() {
-  gulp.src(src_sass)
-      .pipe(plumber())
+  return gulp.src(src_sass)
+      .pipe(plumber({
+        errorHandler: onError
+      }))
       .pipe(sass())
+      .pipe(concat('app.min.css'))
+      .pipe(prefix('last 2 versions'))
       .pipe(gulp.dest(dest_css))
       .pipe(minify_css())
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest(dest_css))
+      // .pipe(notify({ message: 'Hello world, we are done!' }))
 });
 
 // ----------------------------------------------------------------
+
 // Compile JS
 gulp.task('js', function() {
-  gulp.src(src_js)
+  return gulp.src(src_js)
       .pipe(plumber())
       .pipe(uglify())
       .pipe(concat('app.min.js'))
+      .pipe(sourcemaps.init())
+      .pipe(sourcemaps.write())
       .pipe(gulp.dest(dest_js))
 });
 
